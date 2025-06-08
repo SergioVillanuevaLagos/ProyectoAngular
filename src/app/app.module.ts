d349cd8dc7b0f735663fd56e2c815eb7e4f4538f
SergioVillanuevaLagos <sergio.villanueva2101@alumnos.ubiobio.cl>
2025-06-07 22:26:26 -0400

    Creacion de tipo home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { NavbarComponentComponent } from './shared/components/navbar-component/navbar-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavbarComponentComponent
   ],
   imports: [
     BrowserModule,
