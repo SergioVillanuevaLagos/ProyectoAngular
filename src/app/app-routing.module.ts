6fc415d9f1a8b109711f5cd2e9b6cbcbba200c7c
SergioVillanuevaLagos <sergio.villanueva2101@alumnos.ubiobio.cl>
2025-06-12 18:42:39 -0400

    cambios necesarios
    Cabios de archivos necesarios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,10 +5,11 @@ import { DetalleLocacionComponent } from './shared/components/detalle-locacion/d
 import { HomeComponent } from './views/home/home.component';
 import { AdminReportesComponent } from './shared/components/admin-reportes/admin-reportes.component';
 import { AdminLocacionesComponent } from './shared/components/admin-locaciones/admin-locaciones.component';
+import { DetalleProductoComponent } from './views/detalle-producto/detalle-producto.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'locaciones', component: HomeComponent },
+  { path: 'locaciones', component: DetalleProductoComponent },
   { path: 'detalle-locacion/:id', component: DetalleLocacionComponent },
   { path: 'admin-reportes', component: AdminReportesComponent },
   {path: 'admin-locacion', component: AdminLocacionesComponent}
--- src/app/views/detalle-producto/detalle-producto.component.ts
+++ src/app/views/detalle-producto/detalle-producto.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { LocacionesService } from '../../services/locaciones.service';
+import { Locacion } from '../../models/locacion.model';
 
 @Component({
   selector: 'app-detalle-producto',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class DetalleProductoComponent {
 
+    title = 'ProyectoAngular';
+    filteredLocaciones: Locacion[] = [];
+  allLocaciones: Locacion[] = [];
+
+  constructor(private locacionesService: LocacionesService) {}
+
+  ngOnInit(): void {
+    this.locacionesService.getLocaciones().subscribe(data => {
+      this.allLocaciones = data;
+      this.filteredLocaciones = data;
+    });
+  }
+
+  applyFilters(filters: any) {
+    this.filteredLocaciones = this.allLocaciones.filter(loc => {
+      const matchesSearch =
+        loc.Ubicacion.toLowerCase().includes(filters.searchTerm) ||
+        loc.Descripcion.toLowerCase().includes(filters.searchTerm);
+
+      const matchesPrice =
+        (filters.minPrice == null || loc.PrecioMensual >= filters.minPrice) &&
+        (filters.maxPrice == null || loc.PrecioMensual <= filters.maxPrice);
+
+      const matchesHabitaciones =
+        filters.habitaciones == null || loc.Habitaciones === filters.habitaciones;
+
+      return matchesSearch && matchesPrice && matchesHabitaciones;
+    });
+  }
+
+
 }
+
+
