b5bd38872f01dbad05ec8b5d91cec28eef55e36d
celso <celso.san2101@alumnos.ubiobio.cl>
2025-07-22 17:35:50 -0400

    el login esta listando los datos en perfil

--- src/app/user/user-profile/user-profile.component.ts
+++ src/app/user/user-profile/user-profile.component.ts
@@ -9,36 +9,18 @@ import { UserService } from '../user.service';
 export class UserProfileComponent implements OnInit {
   user: any;
   sidebarOpen = true;
+
   constructor(private userService: UserService) {}
-  
-   ngOnInit(): void {
-    const userId = 1; // Cambiar esto luego por el ID desde login/token
 
-    
-    //Perfil de usuario simulado
-    //Perfil de usuario simulado
-      this.user = {
-    nombre: 'Juan',
-    apellidoPaterno: 'Pérez',
-    apellidoMaterno: 'García',
-    correo: 'juan.perez@example.com',
-    rol: 'Arrendador',
-    locaciones: [
-      { descripcion: 'Departamento céntrico', ubicacion: 'Chillan', precio: 120000 }
-    ],
-  favoritos: [
-    { descripcion: 'Casa en la playa', ubicacion: 'Viña del Mar', precio: 250000 },
-    { descripcion: 'Loft moderno', ubicacion: 'Santiago', precio: 180000 }
-  ]
-
-  };
-  //Perfil de usuario simulado
-  //Perfil de usuario simulado
-
-
-    this.userService.getUserProfile(userId).subscribe({
-      next: data => this.user = data,
-      error: err => console.error('Error al cargar perfil', err)
-    });
+  ngOnInit(): void {
+    const user = localStorage.getItem('user');
+    if (user) {
+      this.user = JSON.parse(user);
+      // Si quieres obtener datos actualizados desde el backend:
+      // this.userService.getUserProfile(this.user.IDUsuario).subscribe({
+      //   next: data => this.user = data,
+      //   error: err => console.error('Error al cargar perfil', err)
+      // });
+    }
   }
 }
